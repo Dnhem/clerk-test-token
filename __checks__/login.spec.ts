96c9d1d1a801a0ad2d48c7358a33465286c6bf39
Dana Nhem <dana@Danas-MacBook-Pro.local>
2024-04-26 23:41:18 +0200

    first commit

--- __checks__/login.spec.ts
+++ __checks__/login.spec.ts
@@ -3,11 +3,13 @@ import { test, expect } from "@playwright/test";
 test.describe("app", () => {
   test("sign in", async ({ page }) => {
     //   await setupClerkTestingToken({ page });
+    await page.goto("http://localhost:5173");
+    await expect(page).toHaveTitle(/Vite + React + TS/);
   });
 
-  test("sign up", async ({ page }) => {
-    //   await setupClerkTestingToken({ page });
-  });
+  //   test("sign up", async ({ page }) => {
+  //   await setupClerkTestingToken({ page });
+  //   });
 });
 
 // Setup a host / Checkly Agent and Private Location if necessary
