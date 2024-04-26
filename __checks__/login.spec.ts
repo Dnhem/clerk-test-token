475411a07611f25b5bbffa2ba463cfeb92523323
Dana Nhem <dana@Danas-MacBook-Pro.local>
2024-04-26 23:22:54 +0200

    first commit

---
+++ __checks__/login.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("app", () => {
+  test("sign in", async ({ page }) => {
+    //   await setupClerkTestingToken({ page });
+  });
+
+  test("sign up", async ({ page }) => {
+    //   await setupClerkTestingToken({ page });
+  });
+});
+
+// Setup a host / Checkly Agent and Private Location if necessary
